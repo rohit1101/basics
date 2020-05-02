230377280138a2922dab4045c69b9431defe91c1
Rohit <srohit1101@gmail.com>
2020-05-02 22:20:11 +0530

    timer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
+import "./index.css";
 // import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
 
-function format({ firstName, lastName }) {
-  return `${firstName} ${lastName}`;
+function rend() {
+  return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 
-const user = {
-  firstName: "Rohit",
-  lastName: "S",
-};
+setInterval(() => {
+  ReactDOM.render(rend(), document.querySelector("#root"));
+}, 1000);
+
+// function format({ firstName, lastName }) {
+//   return `${firstName} ${lastName}`;
+// }
+
+// const user = {
+//   firstName: "Rohit",
+//   lastName: "S",
+// };
+
+// function greeting(user) {
+//   if (user) {
+//     return (
+//       <div>
+//         <h1 className="head">I am {format(user)} and excited to learn React</h1>
+//         <h1 className="head">React Docs are awesome!</h1>
+//       </div>
+//     );
+//   } else {
+//     return <h1>Hi Stalker!</h1>;
+//   }
+// }
 
 // const name = "Rohit";
-const element = <h1>I am {format(user)}</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+// const element = <h1>I am {format(user)}</h1>;
+// ReactDOM.render(greeting(user), document.getElementById("root"));
+
+// timer
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
